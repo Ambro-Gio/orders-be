c5224a5e234482f3ebc24d3967b39732d2fdb034
Ambro-Gio <giorgio.ambrosi@live.com>
2025-02-01 17:49:21 +0100

    refactor: remove legacy

--- orders-be/routes/api.php
+++ orders-be/routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\checkUserRole;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(OrderController::class)
